d4014106f366d70cc45b8df269eebbf486b95f6e
Leia Marie Spagnola <leia@meta.com>
2025-06-13 14:41:33 -0700

    add header, footer, and fonts

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,18 @@
+import headerStyles from "../css/Header.module.css";
+import footerStyles from "../css/Footer.module.css";
+
 const HomePage = () => {
-    return <h1>Home</h1>;
+    return (
+        <>
+            <header>
+                <h1>kudoboard</h1>
+            </header>
+            <main></main>
+            <footer>
+                <p>Leia Spagnola 2025</p>
+            </footer>
+        </>
+    );
 };
 
 export default HomePage;
