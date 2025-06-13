2c5e96cd47a634b99575b23b7cc0d21174921712
Leia Marie Spagnola <leia@meta.com>
2025-06-13 16:18:33 -0700

    implement filtering boards

--- src/components/BoardGrid.jsx
+++ src/components/BoardGrid.jsx
@@ -1,55 +1,29 @@
-import { useEffect, useState } from "react";
 import styles from "../css/BoardGrid.module.css";
 import BoardCover from "./BoardCover";
 
-const BoardGrid = (props) => {
-    // array of data representing each board
-    const [boards, setBoards] = useState(Array());
-
-    // placeholder data set on page load
-    const cowGif =
-        "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzRoYnc0MWFjcWhmY2FzMmV6ODVoMzR1djkyeWNnN3VzbWo5cW1mNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QIma0G0GMja3LrGVYL/giphy.gif";
-    const cowAlt = "Pixel art of a cartoon cow driving a motorcycle";
-
-    useEffect(
-        () =>
-            setBoards([
-                {
-                    id: 0,
-                    title: "Teacher Appreciation",
-                    category: "Thank you",
-                    imgSrc: cowGif,
-                    imgAlt: cowAlt,
-                    author: null,
-                    cards: [],
-                },
-                {
-                    id: 1,
-                    title: "Motivation",
-                    category: "Inspiration",
-                    imgSrc: cowGif,
-                    imgAlt: cowAlt,
-                    author: "Ann",
-                    cards: [],
-                },
-                {
-                    id: 2,
-                    title: "Happy Birthday",
-                    category: "Celebration",
-                    imgSrc: cowGif,
-                    imgAlt: cowAlt,
-                    author: null,
-                    cards: [],
-                },
-            ]),
-        [],
-    );
+const BoardGrid = ({ filterValue, boards }) => {
+    const getFilteredBoards = () => {
+        switch (filterValue) {
+            case "Recent":
+                return boards
+                    .toSorted(
+                        (a, b) => new Date(b.created) - new Date(a.created),
+                    )
+                    .slice(0, 6);
+            case "All":
+                return boards;
+            default:
+                return boards.filter((board) => board.category === filterValue);
+        }
+    };
 
     return (
         <section className={styles["board-grid"]}>
-            {boards.map(board => 
-                <BoardCover key={board.id} board={board}></BoardCover>
-            )}
+            {getFilteredBoards().map((board) => (
+                <BoardCover
+                    key={board.id}
+                    board={board}></BoardCover>
+            ))}
         </section>
     );
 };
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,15 +1,70 @@
 import headerStyles from "../css/Header.module.css";
 import footerStyles from "../css/Footer.module.css";
 import BoardGrid from "./BoardGrid";
+import FilterPicker from "./FilterPicker";
+import { useState, useEffect } from "react";
 
 const HomePage = () => {
+    // current filter
+    const [filterValue, setFilterValue] = useState("Recent");
+
+    // array of data representing each board
+    const [boards, setBoards] = useState(Array());
+
+    // placeholder data set on page load
+    const cowGif =
+        "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzRoYnc0MWFjcWhmY2FzMmV6ODVoMzR1djkyeWNnN3VzbWo5cW1mNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QIma0G0GMja3LrGVYL/giphy.gif";
+    const cowAlt = "Pixel art of a cartoon cow driving a motorcycle";
+
+    useEffect(
+        () =>
+            setBoards([
+                {
+                    id: 0,
+                    created: "2025-05-10",
+                    title: "Teacher Appreciation",
+                    category: "Thank you",
+                    imgSrc: cowGif,
+                    imgAlt: cowAlt,
+                    author: null,
+                    cards: [],
+                },
+                {
+                    id: 1,
+                    created: "2023-12-11",
+                    title: "Motivation",
+                    category: "Inspiration",
+                    imgSrc: cowGif,
+                    imgAlt: cowAlt,
+                    author: "Ann",
+                    cards: [],
+                },
+                {
+                    id: 2,
+                    created: "2024-08-20",
+                    title: "Happy Birthday",
+                    category: "Celebration",
+                    imgSrc: cowGif,
+                    imgAlt: cowAlt,
+                    author: null,
+                    cards: [],
+                },
+            ]),
+        [],
+    );
+
     return (
         <>
             <header>
                 <h1>kudoboard</h1>
             </header>
             <main>
-                <BoardGrid></BoardGrid>
+                <FilterPicker
+                    filterValue={filterValue}
+                    updateFilterValue={setFilterValue}></FilterPicker>
+                <BoardGrid
+                    filterValue={filterValue}
+                    boards={boards}></BoardGrid>
             </main>
             <footer>
                 <p>Leia Spagnola 2025</p>
