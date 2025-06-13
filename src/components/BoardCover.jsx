bf2769e00afc493308167ee6d23f1d60729693cf
Leia Marie Spagnola <leia@meta.com>
2025-06-13 15:38:46 -0700

    dynamically load board cards; modify styling

--- src/components/BoardCover.jsx
+++ src/components/BoardCover.jsx
@@ -1,13 +1,15 @@
-import styles from "../css/BoardCover.module.css"
+import styles from "../css/BoardCover.module.css";
 
-const BoardCover = ({ title, imgSrc, imgAlt }) => {
-
+const BoardCover = ({ board }) => {
     return (
         <div className={styles["board-cover"]}>
-            <img className={styles["board-cover-gif"]} src={imgSrc} alt={imgAlt}></img>
-            <h2 className={styles["board-cover-title"]}>{title}</h2>
+            <img
+                className={styles["board-cover-gif"]}
+                src={board.imgSrc}
+                alt={board.imgAlt}></img>
+            <h2 className={styles["board-cover-title"]}>{board.title}</h2>
         </div>
-    )
-}
+    );
+};
 
 export default BoardCover;
--- src/components/BoardGrid.jsx
+++ src/components/BoardGrid.jsx
@@ -1,23 +1,57 @@
+import { useEffect, useState } from "react";
 import styles from "../css/BoardGrid.module.css";
 import BoardCover from "./BoardCover";
 
 const BoardGrid = (props) => {
+    // array of data representing each board
+    const [boards, setBoards] = useState(Array());
 
-    // placeholder
-    const cowGif = "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzRoYnc0MWFjcWhmY2FzMmV6ODVoMzR1djkyeWNnN3VzbWo5cW1mNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QIma0G0GMja3LrGVYL/giphy.gif";
-    const cowAlt = "Pixel art of a cartoon cow driving a motorcycle"
+    // placeholder data set on page load
+    const cowGif =
+        "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzRoYnc0MWFjcWhmY2FzMmV6ODVoMzR1djkyeWNnN3VzbWo5cW1mNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QIma0G0GMja3LrGVYL/giphy.gif";
+    const cowAlt = "Pixel art of a cartoon cow driving a motorcycle";
+
+    useEffect(
+        () =>
+            setBoards([
+                {
+                    id: 0,
+                    title: "Teacher Appreciation",
+                    category: "Thank you",
+                    imgSrc: cowGif,
+                    imgAlt: cowAlt,
+                    author: null,
+                    cards: [],
+                },
+                {
+                    id: 1,
+                    title: "Motivation",
+                    category: "Inspiration",
+                    imgSrc: cowGif,
+                    imgAlt: cowAlt,
+                    author: "Ann",
+                    cards: [],
+                },
+                {
+                    id: 2,
+                    title: "Happy Birthday",
+                    category: "Celebration",
+                    imgSrc: cowGif,
+                    imgAlt: cowAlt,
+                    author: null,
+                    cards: [],
+                },
+            ]),
+        [],
+    );
 
     return (
         <section className={styles["board-grid"]}>
-            <BoardCover title="Title" imgSrc={cowGif} imgAlt={cowAlt}></BoardCover>
-            <BoardCover title="Hello" imgSrc={cowGif} imgAlt={cowAlt}></BoardCover>
-            <BoardCover title="Title" imgSrc={cowGif} imgAlt={cowAlt}></BoardCover>
-            <BoardCover title="Title" imgSrc={cowGif} imgAlt={cowAlt}></BoardCover>
-            <BoardCover title="Title" imgSrc={cowGif} imgAlt={cowAlt}></BoardCover>
-            <BoardCover title="Title" imgSrc={cowGif} imgAlt={cowAlt}></BoardCover>
-            <BoardCover title="Title" imgSrc={cowGif} imgAlt={cowAlt}></BoardCover>
+            {boards.map(board => 
+                <BoardCover key={board.id} board={board}></BoardCover>
+            )}
         </section>
-    )
-}
+    );
+};
 
 export default BoardGrid;
