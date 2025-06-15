9495d339366e8667f8f9e4c051f5d5cb964ca4a4
Leia Marie Spagnola <leia@meta.com>
2025-06-15 16:28:53 -0700

    implement fetching boards from database in homepage view

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
 
 import { PrismaClient } from "./src/generated/prisma/client.js";
@@ -10,5 +10,6 @@ const prisma = new PrismaClient();
 
 app.get("/boards", async (req, res) => {
     const boards = await prisma.board.findMany();
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.json(boards);
-})
+});
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -15,47 +15,15 @@ const HomePage = () => {
     // array of data representing each board
     const [boards, setBoards] = useState(Array());
 
-    // placeholder data set on page load
-    const cowGif =
-        "https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExNzRoYnc0MWFjcWhmY2FzMmV6ODVoMzR1djkyeWNnN3VzbWo5cW1mNCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QIma0G0GMja3LrGVYL/giphy.gif";
-    const cowAlt = "Pixel art of a cartoon cow driving a motorcycle";
+    // load boards from running server (connection to database)
+    const loadBoards = () => {
+        fetch("http://localhost:3000/boards")
+            .then((response) => response.json())
+            .then((json) => setBoards(json))
+            .catch((error) => console.error(`Error fetching boards: ${error}`));
+    };
 
-    useEffect(
-        () =>
-            setBoards([
-                {
-                    id: 0,
-                    created: "2025-05-10",
-                    title: "Teacher Appreciation",
-                    category: "Thank you",
-                    imgSrc: cowGif,
-                    imgAlt: cowAlt,
-                    author: null,
-                    cards: [],
-                },
-                {
-                    id: 1,
-                    created: "2023-12-11",
-                    title: "Motivation",
-                    category: "Inspiration",
-                    imgSrc: cowGif,
-                    imgAlt: cowAlt,
-                    author: "Ann",
-                    cards: [],
-                },
-                {
-                    id: 2,
-                    created: "2024-08-20",
-                    title: "Happy Birthday",
-                    category: "Celebration",
-                    imgSrc: cowGif,
-                    imgAlt: cowAlt,
-                    author: null,
-                    cards: [],
-                },
-            ]),
-        [],
-    );
+    useEffect(loadBoards, []);
 
     return (
         <>
